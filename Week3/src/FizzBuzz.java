a10bb9cb7683e02b2b677fdfbb548043b0293b1f
malteserteresa <tess_the_titan@hotmail.com>
2018-10-16 19:55:12 +0200

    Cleaned working tree and added new FB impl

--- Week3/src/FizzBuzz.java
+++ Week3/src/FizzBuzz.java
@@ -1,17 +1,24 @@
-
 public class FizzBuzz {
 
 	public static void main(String[] args) {
 
 		for (int i = 1; i <= 100; i++)
 
-			if (i % 3 == 0) {
-				System.out.println("Fizz");
-			} else if (i % 5 == 0) {
-				System.out.println("Buzz");
-			} else if (i % 3 == 0 && i % 5 == 0) {
-				System.out.println("FizzBuzz");
-			} else
-				System.out.println(i + " ");
+			switch (int i)
+			{
+			case 1: (i % 3 == 0)
+			System.out.print("Fizz")
+			break;
+			
+			case 2: (i % 5 == 0)
+			System.out.print("Buzz")
+			break;
+			
+			case 3: ( i % 3 == 0 && i % 5 == 0)
+			System.out.print("FizzBuzz")
+			break;
+				}
 	}
 }
+
+
