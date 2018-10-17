b84546ed6c5fa60b78c2ab8461274474ea91f501
malteserteresa <tess_the_titan@hotmail.com>
2018-10-17 22:19:00 +0200

    Control statements work

--- Week3/src/work/CheckerBoard.java
+++ Week3/src/work/CheckerBoard.java
@@ -4,27 +4,29 @@ public class CheckerBoard {
 	/*
 	 * Play time! 
 	 * Change the < to <=, what happens?
-	 * What happesns when you change the while to a for loop
+	 * change j<10 for j<=i
+	 * 
 	 */
 	
 	public static void main(String[] args) {
 		
 
-		int i = 0;
-		while(i < 10) {
+	
+		for(int i = 0; i < 10; i++) {
 			for(int j = 0; j < 10; j++) {
 			System.out.print(i);
-			System.out.print("* ");
+			System.out.print("*");
+			System.out.print(j);
+			System.out.print(" ");
 			//System.out.print(" ");
 		//
 			
 			}
 			System.out.println();
-			i++;
+			
 		}
 	}
 	  
 
 }
-	
 
