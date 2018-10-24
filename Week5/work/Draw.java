5be682a45406221c34e88813bc1985755ee7b92c
malteserteresa <tess_the_titan@hotmail.com>
2018-10-24 11:22:37 +0200

    Added bullseye

--- Week5/work/Draw.java
+++ Week5/work/Draw.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 public class Draw {
 	
 	public static void main(String[] args) {
-		Smiley panel = new Smiley();
+		BullsEye panel = new BullsEye();
 		JFrame application = new JFrame();
 		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		application.add(panel);
