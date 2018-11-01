51b5f5b59903b2f4c60c2f29b9fcde218dd6f0ec
malteserteresa <tess_the_titan@hotmail.com>
2018-11-01 09:34:39 +0100

    Mansion task

---
+++ Week5/work/House.java
@@ -0,0 +1,18 @@
+package work;
+
+/**
+ * Direct a person through the Downton Abbey Mansion, giving one line of description as you enter each room.
+ * Focus on the Entrance Hall first, deciding whether to go into the salon, dining room or north library
+ * 
+ * @author tcake
+ *
+ */
+public class House {
+
+	public static void main(String[] args) {
+		// get a starting position
+		// write a method to tell the person where they are (switch)
+		// write a method to ask where they would like to go to 
+	}
+
+}
